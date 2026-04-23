99b0fc8d57a8e4a001a20b3cc0399bcecbff7925
Sean Boots <sean.boots@gmail.com>
2026-04-23 12:28:51 -0700

    Log request details and response times in a separate CSV file

--- matomo_sync_usage_data.R
+++ matomo_sync_usage_data.R
@@ -1,6 +1,41 @@
 source("lib/ckan_helpers.R")
 
-sync_usage_data <- function(offset = 0, limit = 25) {
+
+# Request log -------------------------------------------------------------
+
+request_log <- tribble(
+  ~time, 
+  ~request_number, 
+  ~offset,
+  ~processed,
+  ~updated,
+  ~skipped,
+  ~failed,
+  ~response_time_seconds
+)
+
+# # Logging helper function
+# add_request_log_entry <- function(...) {
+#   
+#   log_text <- str_c(...)
+#   
+#   new_row = tibble_row(
+#     time = now(),
+#     message = log_text
+#   )
+#   
+#   run_log <<- run_log |>
+#     bind_rows(
+#       new_row
+#     )
+#   
+#   cat(log_text, "\n")
+# }
+
+
+# CKAN API call -----------------------------------------------------------
+
+sync_usage_data <- function(offset = 0, limit = 50) {
   
   # Generic CKAN action helper
   response <- ckan_action(
@@ -26,9 +61,12 @@ sync_all_usage_data <- function(starting_offset = 0) {
     
     add_log_entry("Request ", iteration, " starting at ", next_offset)
     
+    request_start_time <- now()
+    
     response <- jsonlite::fromJSON(sync_usage_data(next_offset))
-    has_more <- response$result$has_more
-    next_offset <- response$result$next_offset
+    
+    request_end_time <- now()
+    response_time_seconds <- round(time_length(interval(request_start_time, request_end_time), "seconds"), digits = 2)
     
     add_log_entry(
       "  Processed ", 
@@ -41,6 +79,25 @@ sync_all_usage_data <- function(starting_offset = 0) {
       response$result$failed,
       "."
       )
+    
+    
+    request_log <<- request_log |>
+      bind_rows(
+        tibble_row(
+          time = now(),
+          request_number = iteration,
+          offset = next_offset,
+          processed = response$result$processed,
+          updated = response$result$updated,
+          skipped = response$result$skipped,
+          failed = response$result$failed,
+          response_time_seconds = response_time_seconds
+        )
+      )
+    
+    # Determine whether to run again based on the previous response:
+    next_offset <- response$result$next_offset
+    has_more <- response$result$has_more
     
     iteration <- iteration + 1
     
@@ -63,4 +120,5 @@ add_log_entry("End time was: ", run_end_time)
 add_log_entry("Run duration: ", round(time_length(interval(run_start_time, run_end_time), "minutes"), digits = 2), " minutes")
 
 run_log |> write_csv("log/run_log.csv")
+request_log |> write_csv("log/request_log.csv")
 # run_log |> View()
